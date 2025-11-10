eda0ccf12335e0ebf6700fa286d7fd2fc5cbc11b
Lars Kvihaugen <lars@kvihaugen.no>
2025-11-10 17:51:47 +0100

    [main]: Added logging, formatting of files, and deletion of unnecessary objects

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,10 @@ const cwd = process.cwd();
 
 (async () => {
 
-    console.log("🔃 Creating frontend app...");
+    console.log("🚀 Creating frontend app...\n");
+
+
+    console.log("\t- 🔃 Generating Next.js app...");
 
     const emitter = degit("LarsSK06/frontend-template");
 
@@ -20,6 +23,11 @@ const cwd = process.cwd();
     await emitter.clone(tempFolderPath);
 
     execSync("npx create-next-app . --yes --empty --skip-install");
+
+    console.log("\t  ✅ Generated Next.js app!\n");
+
+
+    console.log("\t- 🔃 Cloning files from template...");
 
     for (const objName of [
         "src",
@@ -44,6 +52,48 @@ const cwd = process.cwd();
         );
     }
 
-    console.log("✅ Created frontend app!");
+    console.log("\t  ✅ Cloned files from template!\n");
+
+
+    console.log("\t- 🔃 Formatting files...");
+
+    for (const fileName of [
+        "package.json",
+        "package-lock.json",
+        "tsconfig.json"
+    ]) {
+        const absolutePath = path.join(cwd, fileName);
+
+        if (!fs.existsSync(absolutePath))
+            continue;
+
+        const fileContent = fs.readFileSync(absolutePath).toString();
+
+        fs.writeFileSync(
+            absolutePath,
+            JSON.stringify(JSON.parse(fileContent), null, 4)
+        );
+    }
+
+    console.log("\t  ✅ Formatted files!\n");
+
+
+    console.log("\t- 🔃 Deleting unnecessary files...");
+
+    for (const objName of [
+        "README.md"
+    ]) {
+        const absolutePath = path.join(cwd, objName);
+
+        if (!fs.existsSync(absolutePath))
+            continue;
+
+        const stat = fs.statSync(absolutePath);
+
+        if (stat.isFile()) fs.unlinkSync(absolutePath);
+        else fs.rmSync(absolutePath, { recursive: true, force: true });
+    }
+
+    console.log("\t  ✅ Deleted unnecessary files!");
 
 })();
